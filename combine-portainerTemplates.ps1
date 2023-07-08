8ee1f173fc7b349374ef455de67e073acdd52d87
Mark McInturff <mark.mcinturff@kyndryl.com>
2023-07-07 20:15:04 -0500

    07/07/2023 20:15:03

--- combine-portainerTemplates.ps1
+++ combine-portainerTemplates.ps1
@@ -1,9 +1,10 @@
 cat .\portainer-templates.txt | 
-   Foreach-Object { $a = curl.exe $_  | convertFrom-Json
-      $b = $a.templates | ConvertTo-Json | out-String
-      $global:c = "{
-   `"version`": `"2`",
-   `"templates`":$b
+   Foreach-Object {
+   $a = curl.exe $_  | convertFrom-Json
+      $b = ($a.templates | ConvertTo-Json ).Split("`n")
+      $c = "{
+      `"version`": `"2`",
+      `"templates`":$b
 }"
 
    } #foreach
